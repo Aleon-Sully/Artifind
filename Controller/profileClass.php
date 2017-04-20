3650cc35333edccd843f0cf3ed9c3c3f9df0a575
Deborah Attuah <Deborah Attuah>
2017-04-20 00:55:20 +0000

    added the loadName function

--- Controller/profileClass.php
+++ Controller/profileClass.php
@@ -1,3 +1,16 @@
 <?php
 require_once($_SERVER['DOCUMENT_ROOT'].'/Artifind/Artifind/Database/dbConnectionClass.php');
+
+class profileClass extends dbconnection {
+
+public function loadName ($artid){
+	$this->connect();
+	$sql = "SELECT first_name, last_name FROM artisan where artisan_id=$artid";
+    $this->query($sql);
+    while ($name = $this->fetch()) {
+    echo "<h1>".$name['first_name']. " ".$name['last_name']."</h1>";
+    }
+
+
+}
 ?>
