7c7840f05e889005f4294ba70bb7f41fd5cb8511
CGouanfo <gouanfocynthia@gmail.com>
2017-04-23 19:55:24 +0100

    search 3.0

--- Pages/category.php
+++ Pages/category.php
@@ -14,61 +14,61 @@
 	<link rel="stylesheet" type="text/css" href="../css/mystyle.css">
 
 
-<?php
-include "Database/dbConnectionClass.php";
+	<?php
+	include "Database/dbConnectionClass.php";
 
-$obj = new dbconnection;
+	$obj = new dbconnection;
 
-?>
+	?>
 </head>
 <body>
-<div class="allcontain">
-	<div class="header">
+	<div class="allcontain">
+		<div class="header">
 			<ul class="givusacall">
 				<li>Give us a call : +233 50 121 2329 </li>
 			</ul>
-	</div>
-	<!-- Navbar Up -->
-	<nav class="topnavbar navbar-default topnav">
-		<div class="container">
-			<div class="navbar-header">
-				<button type="button" class="navbar-toggle collapsed toggle-costume" data-toggle="collapse" data-target="#upmenu" aria-expanded="false">
-					<span class="sr-only"> Toggle navigaion</span>
-					<span class="icon-bar"></span>
-					<span class="icon-bar"></span>
-					<span class="icon-bar"></span>
-				</button>
-				<a class="navbar-brand logo" href="#"><img src="../image/Logo.jpg" alt="logo"></a>
+		</div>
+		<!-- Navbar Up -->
+		<nav class="topnavbar navbar-default topnav">
+			<div class="container">
+				<div class="navbar-header">
+					<button type="button" class="navbar-toggle collapsed toggle-costume" data-toggle="collapse" data-target="#upmenu" aria-expanded="false">
+						<span class="sr-only"> Toggle navigaion</span>
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+					</button>
+					<a class="navbar-brand logo" href="#"><img src="../image/Logo.jpg" alt="logo"></a>
+				</div>
 			</div>
-		</div>
-		<div class="collapse navbar-collapse" id="upmenu">
-			<ul class="nav navbar-nav" id="navbarontop">
-				<li class="active"><a href="../index.php">Home</a> </li>
-				<li class="dropdown">
-          			<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Category<span class="caret"></span></a>
-			          <ul class="dropdown-menu">
-			          <?php include "Pages/profession.php"; ?>
-			          	<?php if(getProfession()) :?>
-						 <?php foreach(getProfession() as  $value):	?>
-						 	<li><a href="#"> <?php echo $value["profession"]?></a></li>
+			<div class="collapse navbar-collapse" id="upmenu">
+				<ul class="nav navbar-nav" id="navbarontop">
+					<li class="active"><a href="../index.php">Home</a> </li>
+					<li class="dropdown">
+						<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Category<span class="caret"></span></a>
+						<ul class="dropdown-menu">
+							<?php include "Pages/profession.php"; ?>
+							<?php if(getProfession()) :?>
+								<?php foreach(getProfession() as  $value):	?>
+									<li><a href="#"> <?php echo $value["profession"]?></a></li>
+									
+								<?php endforeach;	?>
+							<?php endif;	?>
 							
-							<?php endforeach;	?>
-			        <?php endif;	?>
-			            
-			          </ul>
-			        </li>
-				<li class="active"><a href="../Register/signUp.php">Artisan? Sign Up</a> </li>
-				<li class="active"><a href="../Login/Sign_in.php">Sign In</a> </li>
-				<li class="active"><a href="../Pages/About.php">About Us</a> </li>
-				<li class="active"><a href="../Contact_us/contactUs.php">Contact Us</a> </li>
-			</ul>
-		</div>
-	</nav>
-</div>
-<script type="text/javascript" src="js/jquery.js"></script>
-<script type="text/javascript" src="js/isotope.js"></script>
-<script type="text/javascript" src="js/myscript.js"></script>
-<script type="text/javascript" src="js/jquery.1.11.js"></script>
-<script type="text/javascript" src="js/bootstrap.js"></script>
+						</ul>
+					</li>
+					<li class="active"><a href="../Register/signUp.php">Artisan? Sign Up</a> </li>
+					<li class="active"><a href="../Login/Sign_in.php">Sign In</a> </li>
+					<li class="active"><a href="../Pages/About.php">About Us</a> </li>
+					<li class="active"><a href="../Contact_us/contactUs.php">Contact Us</a> </li>
+				</ul>
+			</div>
+		</nav>
+	</div>
+	<script type="text/javascript" src="js/jquery.js"></script>
+	<script type="text/javascript" src="js/isotope.js"></script>
+	<script type="text/javascript" src="js/myscript.js"></script>
+	<script type="text/javascript" src="js/jquery.1.11.js"></script>
+	<script type="text/javascript" src="js/bootstrap.js"></script>
 </body>
 </html>
--- Pages/profession.php
+++ Pages/profession.php
@@ -3,14 +3,14 @@
 // include the database
 function getProfession()
 {
- global $obj;	
-$obj->query("SELECT distinct profession FROM artisan ");
-$data = array();
-while($row = $obj->fetch())
-{
-   $data[] = $row;
+	global $obj;	
+	$obj->query("SELECT distinct profession FROM artisan ");
+	$data = array();
+	while($row = $obj->fetch())
+	{
+		$data[] = $row;
 
-}
+	}
 
 	return $data;
 }
